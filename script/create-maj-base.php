21f1c6b75d4fb1987dac71a707d2ca0c33d83c8c
atm-greg <gregory.blemand@atm-consulting.fr>
2020-01-15 16:03:30 +0100

    init db

--- script/create-maj-base.php
+++ script/create-maj-base.php
@@ -70,3 +70,7 @@ $o->init_db_by_vars();
 dol_include_once('/dolifleet/class/vehiculeRental.class.php');
 $o=new dolifleetVehiculeRental($db);
 $o->init_db_by_vars();
+
+dol_include_once('/dolifleet/class/vehiculeOperation.class.php');
+$o=new dolifleetVehiculeOperation($db);
+$o->init_db_by_vars();
