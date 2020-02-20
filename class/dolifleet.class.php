523a7a3868732b548c73119e9f1a71f59ecb1bf0
John Botella <john.botella@atm-consulting.fr>
2020-02-20 10:56:48 +0100

    add method showOutputFieldQuick

--- class/dolifleet.class.php
+++ class/dolifleet.class.php
@@ -459,9 +459,23 @@ class doliFleet extends SeedObject
             elseif ($mode == 5) $res = $statusLabelShort.img_picto($statusLabel, $statusType);
             elseif ($mode == 6) $res = $statusLabel.img_picto($statusLabel, $statusType);
         }
-        
+
         return $res;
     }
+
+	/**
+	 * Return HTML string to show a field into a page
+	 *
+	 * @param  string  $key            Key of attribute
+	 * @param  string  $moreparam      To add more parameters on html input tag
+	 * @param  string  $keysuffix      Prefix string to add into name and id of field (can be used to avoid duplicate names)
+	 * @param  string  $keyprefix      Suffix string to add into name and id of field (can be used to avoid duplicate names)
+	 * @param  mixed   $morecss        Value for css to define size. May also be a numeric.
+	 * @return string
+	 */
+	public function showOutputFieldQuick($key, $moreparam = '', $keysuffix = '', $keyprefix = '', $morecss = ''){
+		return $this->showOutputField($this->fields[$key], $key, $this->{$key}, $moreparam, $keysuffix, $keyprefix, $morecss);
+	}
 }
 
 
